a2d08b9bfcf09ebafe2bdcb4bdd4032a30dbef15
Mike Snodgrass <mikesnodgrass@google.com>
2025-12-28 20:46:10 +0000

    Commit message

--- app/agent.py
+++ app/agent.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# New comment added my Mike Snodgrass
+
 """
 Vendor Spend Analysis Agent with Hybrid Search.
 
